df6005adff295573c576c7dc17e5e289f9cfa336
elicas7 <netotalents@gmail.com>
2026-07-04 13:57:12 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 // backend/db.js — pool de conexão com o PostgreSQL
 const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 
 const { Pool } = require('pg');
 
